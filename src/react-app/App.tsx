89d742967580c07434e404250a63cbffa8fd3a13
yorm <birbnaise@gmail.com>
2026-01-05 04:30:38 -0800

    add finishing touches

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,10 +1,12 @@
 import chibiImage from './assets/chibi.webp'
 import bannerMask from './assets/banner-mask.webp'
-import selfieImage from './assets/selfie2.webp'
+import selfieImage from './assets/selfie.webp'
 import cookoffImage from './assets/proj-cookoff.png'
 import butterfilesImage from './assets/proj-butterfiles.webp'
 import musicPlayerImage from './assets/proj-musicplayer.webp'
+import bunnyImage from './assets/bunny.webp'
 import './App.css'
+import { Style } from 'util'
 
 
 interface CardData {
@@ -19,57 +21,71 @@ const cardsData: CardData[] = [
   {
     title: "Bio",
     category: "> About",
-    description: "My name is April and I'm currently working towards a BA in Computer Science. " +
-      "I love coding and making side projects. " +
-      "When I have my free time, I like drawing and designing.",
+    description: `I'm working towards my BA right now. 
+      When I don't have to stress over exams, 
+      I use my free time to work on side projects, 
+      like the website you're on right now! 
+      Besides coding, I also draw digital art, 
+      like that drawing of me above. `,
     imageUrl: selfieImage,
     pills: ["C++", "C#", "Python", "React", "Unity", "Godot"]
   },
   {
-    title: "Chun & Snuggles Coop Cookoff",
+    title: "Chun & Snuggles Coop Cook-off",
     category: "> Project",
-    description: "Business managment simulation 3D web game about 2 chickens running a burger restaurant.",
+    description: `This is a game I made in Godot 
+      where you play as a chicken running a burger restaurant
+      out of a backyard coop. The two main chicken characters
+      are based on my partner's real-life pet chickens, Chun and Mr. Snuggles!`,
     imageUrl: cookoffImage,
     pills: ["Godot", "Python", "WebGL"]
   },
   {
     title: "Butterfiles",
     category: "> Project",
-    description: "Song search website with filtering options and community interaction for the game Stepmania. In progress.",
+    description: `I'm making a website to catalog and share 
+    songs for the PC game "Stepmania", after realizing how outdated
+    and messy the current options are. It's a big learning experience
+    for me, trying to combine my frontend and backend skills
+    into one big project people would actually use.`,
     imageUrl: butterfilesImage,
     pills: ["React", "Database"]
   },
   {
     title: "Music Player + Clock",
     category: "> Project",
-    description: "Spotify music player and clock on an e-ink screen running off a microcontroller with playback controls. In progress.",
+    description: `I'm using a Raspberry Pi Pico to build a
+    cute little music player/clock as a gift for someone
+    who really loves music. Building physical devices is
+    new territory for me, but it's also been really
+    fun designing every aspect like UI and learning about APIs.`,
     imageUrl: musicPlayerImage,
     pills: ["Raspberry Pi", "Python", "APIs", "HTML/CSS"]
-  },
+  }
 ]
 
+// function animTypewriter(text: string) {
+//   return {
+//     animation: "typewriter 4s steps(" + text.length + 
+//         ") 1s 1 normal both, blinkTextCursor 500ms steps(" + 
+//         text.length + ") infinite normal"
+//   }
+// }
 
-function Watermark() {
-  return (
-    <p className="pill watermark">buncode.dev</p>
-  )
-}
-
-
-// cards
+// ////////// Cards ////////////
 function Card({ category, description, imageUrl, title, pills }: CardData) {
   return (
-    <div className="card">
+    <div className="card hide-scrollbar">
       <p className="card-category">{category}</p>
       <img src={imageUrl} alt={title} className="card-image" />
+
       <section className="card-content">
         <h2 className="card-title">{title}</h2>
         <p className="card-description">{description}</p>
-        {pills.map((pill, index) => (
+        {/* {pills.map((pill, index) => (
           <p key={index} className="pill">{pill}</p>
-        ))}
+        ))} */}
       </section>
-      {/* <Watermark /> */}
     </div>
   )
 }
@@ -88,13 +104,12 @@ function CoverCard() {
         maskPosition: 'top',
         WebkitMaskPosition: 'top',
       }}>
-        <h1 className="cover-title">&gt; April_</h1>
+        <h1 className="cover-title anim-typewriter"> &gt; April_</h1>
         <p>I'm a Comp Sci student studying at UNLV.</p>
         <div className="divider" />
         <a href="https://github.com/buncode-dev"><p className="pill">GitHub</p></a>
         <p className="pill">Linkedin</p>
         <p className="pill">Handshake</p>
-        {/* <Watermark /> */}
       </div>
     </div>
   )
@@ -104,22 +119,25 @@ function CreditsCard() {
   return (
     <div className="card credits-card">
       <div className='card-content'>
+        <img className='bunny-logo' src={bunnyImage} alt="Bunny Logo" />
+
         <h2 className="card-title">Credits</h2>
-        <p>Made in React + Vite</p>
         <p>Website by me</p>
         <p>Art and design by me</p>
-        <p>Background image by <a href="https://unsplash.com/@anniespratt">Annie Spratt</a> on Unsplash</p>
+        <p>Background image by Daan Stevens on Pexels</p>
       </div>
-      <Watermark />
+      {/* <p className="pill watermark">buncode.dev</p> */}
     </div>
   )
 }
 
 
+// ////////// App ////////////
 function App() {
   return (
     <>
       <main>
+        {/* <PageDots /> */}
         <div className="cards-stack">
           <CoverCard />
           {cardsData.map((card, index) => (
